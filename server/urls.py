d3c34723106a48883829dcb39b171e5ae8169aae
Pakin Boonchoopirach <moobinkung@windowslive.com>
2017-01-07 22:30:29 +0700

    - moved views in old file to test.py in views directory.

--- server/urls.py
+++ server/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import user
+from .views import user, test
 
 app_name = 'server'
 
 urlpatterns = [
     url(r'^login', user.user_login, name='login'),
     url(r'^logout', user.user_logout, name='logout'),
-    url(r'^hello/$', views.hello.as_view()),
-    url(r'^hello/(?P<pk>[0-9]+)/$', views.snippet_detail.as_view()),
+    url(r'^hello/$', test.hello.as_view()),
+    url(r'^hello/(?P<pk>[0-9]+)/$', test.snippet_detail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
