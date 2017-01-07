3cd1a015bbf73469e2013540d4c489690cd76a71
Pakin Boonchoopirach <moobinkung@windowslive.com>
2017-01-07 21:30:13 +0700

    - integrated django-rest to django

--- DogFinderServer/settings.py
+++ DogFinderServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=vn9rkb(9r&1+%65q1d!n^lyunuy@w1s!-((deu7lukdha(b@p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.195.134']
 
 
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- server/urls.py
+++ server/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 app_name = 'server'
+
 urlpatterns = [
     url(r'^add_user', views.add_user, name='add_user'),
     url(r'^login', views.user_login, name='login'),
-    url(r'^hello', views.hello),
+    url(r'^hello/$', views.hello.as_view()),
+    url(r'^hello/(?P<pk>[0-9]+)/$', views.snippet_detail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
